26fd3ba867d9c50efeaa3ebdd568ab1ea199a507
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2020-02-07 18:11:38 +0300

    initial commit

---
+++ lib/Git/Repository/Plugin/Diff.pm
@@ -0,0 +1,39 @@
+package Git::Repository::Plugin::Diff;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Git::Repository::Plugin::Diff - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Git::Repository::Plugin::Diff;
+
+=head1 DESCRIPTION
+
+Git::Repository::Plugin::Diff is
+
+=head1 AUTHOR
+
+d.tarasov E<lt>d.tarasov@corp.mail.ruE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2020- d.tarasov
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
